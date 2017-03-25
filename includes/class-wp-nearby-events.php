coreymckrill/nearby-wordpress-events

commit cd5029eabce2164f376c29946a1a09a43e92e83c

Always require passed location to simplify get_events_transient_key
The reason that `get_events_transient_key()` could either accept a passed `$location` or use `$this->user_location` was because `$this->user_location` is outdated when the location changes, until the class is re-initialized on the next page load. Because of that, if `$this->user_location` were always used, then the key would be different when cached events were stored than when they were retrieved.

To avoid that behavior, `get_transient_event_key()` could optionally accept the passed `$location` and use that instead. That was a code smell, though, because this function was aware of, and interacting with the controller's state, rather than being a pure model. To make it worse, that fact wasn't documented, making debugging more difficult and time consuming.

It's simpler and more architectually-consistent to just have `get_transient_event_key()` always do the same thing, and let the calling function decide which data to pass based on it's higher-level knowledge of the application state.

includes/class-wp-nearby-events.php
@@ -164,20 +164,23 @@ class WP_Nearby_Events {
 	/**
 	 * Generate a transient key based on user location
 	 *
-	 * This is only one line, but it's a separate function because it's used multiple
-	 * times, and having it abstracted keeps the logic consistent and DRY, which is
-	 * less prone to errors.
+	 * This could be reduced to a one-liner in the calling functions, but it's
+	 * intentionally a separate function because it's called from multiple
+	 * locations, and having it abstracted keeps the logic consistent and DRY,
+	 * which is less prone to errors.
 	 *
-	 * @param bool|array $location
+	 * @param array $location Should contain 'latitude' and 'longitude' indexes
 	 *
-	 * @return string
+	 * @return bool|string `false` on failure, or a string on success
 	 */
-	private function get_events_transient_key( $location = false ) {
-		$data = false === $location ?
-			$this->user_location['latitude'] . $this->user_location['longitude'] :
-			$location['latitude'] . $location['longitude'];
+	private function get_events_transient_key( $location ) {
+		$key = false;
 
-		return 'nearbywp-' . md5( $data );
+		if ( isset( $location['latitude'], $location['longitude'] ) ) {
+			$key = 'nearbywp-' . md5( $location['latitude'] . $location['longitude'] );
+		}
+
+		return $key;
 	}
 
 	/**
@@ -201,6 +204,6 @@ class WP_Nearby_Events {
 	 *                     and `events` items on success
 	 */
 	public function get_cached_events() {
-		return get_transient( $this->get_events_transient_key() );
+		return get_transient( $this->get_events_transient_key( $this->user_location ) );
 	}
 }

commit b3f57d2654ac86b29cc93407afc460b08fa18369

Only set the transient when a valid key exists
Otherwise it'll create a transient named `_transient_`. This bug was introduced in cd5029eabce2164f376c29946a1a09a43e92e83c.

includes/class-wp-nearby-events.php
@@ -37,7 +37,7 @@ class WP_Nearby_Events {
 	 * location, these will be immediately returned.
 	 *
 	 * If not, this method will send a request to the Events API with location data.
-	 * The API will send back a recongized location based on the data, along with
+	 * The API will send back a recognized location based on the data, along with
 	 * nearby events.
 	 *
 	 * @param string $location_search Optional search string to help determine the location.
@@ -201,10 +201,15 @@ class WP_Nearby_Events {
 	 * @return bool
 	 */
 	private function cache_events( $events ) {
+		$set              = false;
 		$transient_key    = $this->get_events_transient_key( $events['location'] );
 		$cache_expiration = isset( $events['ttl'] ) ? absint( $events['ttl'] ) : HOUR_IN_SECONDS * 12;
 
-		return set_transient( $transient_key, $events, $cache_expiration );
+		if ( $transient_key ) {
+			$set = set_transient( $transient_key, $events, $cache_expiration );
+		}
+
+		return $set;
 	}
 
 	/**
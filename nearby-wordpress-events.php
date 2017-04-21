5f0367d97562ec286b87c4a95e59592dabd88d7e
Ian Dunn <ian@iandunn.name>
2017-04-20 19:31:01 -0700

    Include `deprecated.php` as a last-resort noop option for the merge to Core
    See #31

--- nearby-wordpress-events.php
+++ nearby-wordpress-events.php
@@ -23,6 +23,7 @@ if ( ! is_admin() ) {
 // Prevent the plugin from loading if this has been merged to Core
 require_once( ABSPATH . '/wp-admin/includes/dashboard.php'    );
 require_once( ABSPATH . '/wp-admin/includes/ajax-actions.php' );
+require_once( ABSPATH . '/wp-admin/includes/deprecated.php'   );
 if ( function_exists( 'wp_fetch_nearby_events' ) || function_exists( 'wp_ajax_fetch_nearby_events' ) ) {
 	return;
 }
@@ -95,6 +96,8 @@ function nearbywp_enqueue_scripts() {
  * @todo during the merge to Core, this function -- or possibly WP_Nearby_Events::get_events() -- must be renamed
  *       to `wp_fetch_nearby_events` or `wp_ajax_fetch_nearby_events` to preserve back-compat with the early
  *       return at the start of this file. Otherwise, sites with the plugin installed could break.
+ *       If neither of those names is desired then, worse case, we can just put a stub for one of them in
+ *       `wp-admin/includes/deprecated.php`.
  */
 function nearbywp_ajax_get_events() {
 	check_ajax_referer( 'nearbywp_events' );

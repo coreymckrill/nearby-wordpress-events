79fbb03f7970d66e99a993d98199c1641e192c13
Ian Dunn <ian@iandunn.name>
2017-05-01 13:40:13 -0700

    Document why the specific cities were chosen as examples

--- includes/script-loader.php
+++ includes/script-loader.php
@@ -47,6 +47,12 @@ function nearbywp_get_inline_script_data() {
 			/* translators: %s is the detailed error message. */
 			'errorOccurredPleaseTryAgain' => __( 'An error occured while trying to retrieve events. Please try again.', 'nearby-wp-events' ),
 
+			/*
+			 * These specific examples were chosen to highlight the fact that a
+			 * state is not needed, even for cities whose name is not unique.
+			 * It would be too cumbersome to include that in the instructions
+			 * to the user, so it's left as an implication.
+			 */
 			/* translators: %s is the name of the city we couldn't locate. Replace the examples with cities in your locale, but test that they match the expected location before including them. Use endonyms (native locale names) whenever possible. */
 			'couldNotLocateCity' => __( 'We couldn\'t locate <strong><em>%1$s</em></strong>. Please try another nearby city. For example: <em>Kansas City; Springfield; Portland<em>.', 'nearby-wp-events' ),
 		)

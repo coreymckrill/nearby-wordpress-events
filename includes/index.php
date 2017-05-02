1b0549bebf7fbf9bcd3eed425e82dc5ba3f31af2
Ian Dunn <ian@iandunn.name>
2017-05-02 13:07:22 -0700

    Enqueue stylesheet earlier to avoid flash of unstyled content

--- includes/index.php
+++ includes/index.php
@@ -5,9 +5,15 @@ defined( 'WPINC' ) || die();
 /**
  * Enqueue dashboard widget scripts and styles
  *
+ * @param string $hook_suffix The slug of the current screen
+ *
  * @todo This function can be removed during the merge, since Core already enqueues these files.
  */
-function nearbywp_enqueue_scripts() {
+function nearbywp_enqueue_scripts( $hook_suffix ) {
+	if ( 'index.php' !== $hook_suffix ) {
+		return;
+	}
+
 	wp_enqueue_style( 'nearbywp' );
 	wp_enqueue_script( 'nearbywp' );
 }
